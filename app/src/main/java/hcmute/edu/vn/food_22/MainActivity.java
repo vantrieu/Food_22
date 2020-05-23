8226fd0db9fa61c18ea89c42f3bcccdf88d4c61a
vantrieu <nguyenvantrieu.it@gmail.com>
2020-05-23 17:20:28 +0700

    Create FlashScreen

--- app/src/main/java/hcmute/edu/vn/food_22/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/food_22/MainActivity.java
@@ -2,10 +2,16 @@ package hcmute.edu.vn.food_22;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView txtVersion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
