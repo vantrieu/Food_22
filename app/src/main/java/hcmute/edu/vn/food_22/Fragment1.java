d2ad62dd8c34ec38522c5f5f751278c8f59de13c
vantrieu <nguyenvantrieu.it@gmail.com>
2020-06-12 07:58:22 +0700

    Update Fragment

--- app/src/main/java/hcmute/edu/vn/food_22/Fragment1.java
+++ app/src/main/java/hcmute/edu/vn/food_22/Fragment1.java
@@ -5,18 +5,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Fragment1 extends Fragment {
+    private TextView txttemp;
 
-    //private ViewGroup mContainer;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //mContainer = container;
-        return inflater.inflate(R.layout.fragment_1, container, false);
+        View view = inflater.inflate(R.layout.fragment_1, container, false);
+        txttemp = (TextView) view.findViewById(R.id.txtTest);
+        Bundle bundle = getArguments();
+        Toast.makeText(container.getContext(), String.valueOf(bundle.getInt("res_id")), Toast.LENGTH_SHORT).show();
+        return view;
     }
 }
--- app/src/main/java/hcmute/edu/vn/food_22/MenuRestaurantActivity.java
+++ app/src/main/java/hcmute/edu/vn/food_22/MenuRestaurantActivity.java
@@ -6,21 +6,20 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MenuRestaurantActivity extends AppCompatActivity {
 
     private Button btnThucDon, btnAnh;
-    private TextView btnRes_name;
+    private TextView txtRes_name;
     private ImageView imgBack;
     private int Res_id;
-    private FrameLayout frameLayout;
+    private Database database = new Database(this, "foody.db", null, 1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +28,13 @@ public class MenuRestaurantActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         Res_id = intent.getExtras().getInt("res_id");
-        Toast.makeText(this, String.valueOf(Res_id), Toast.LENGTH_SHORT).show();
 
         btnThucDon = (Button) findViewById(R.id.button_menu_text);
         btnAnh = (Button) findViewById(R.id.button_menu_image);
         imgBack = (ImageView) findViewById(R.id.imgbutton_back);
-        frameLayout = (FrameLayout) findViewById(R.id.container_body);
+        txtRes_name = (TextView) findViewById(R.id.txtRes_id);
+
+        getData();
 
         btnThucDon.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,24 +61,22 @@ public class MenuRestaurantActivity extends AppCompatActivity {
             }
         });
 
-        initFragment();
-
-
+        addFragment(new Fragment1());
     }
-
-    private void initFragment() {
-        Fragment1 firstFragment = new Fragment1();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction ft = fragmentManager.beginTransaction();
-        ft.replace(R.id.container_body, firstFragment);
-        ft.commit();
+    protected void getData(){
+        Cursor dataRes = database.GetData("SELECT res_name FROM Restaurant WHERE Restaurant.res_id =" + Res_id);
+        dataRes.moveToFirst();
+        txtRes_name.setText(dataRes.getString(0));
     }
 
     protected void addFragment(Fragment fragment) {
         FragmentManager fmgr = getSupportFragmentManager();
         FragmentTransaction ft = fmgr.beginTransaction();
+        Bundle bundle = new Bundle();
+        bundle.putInt("res_id", Res_id);
+        fragment.setArguments(bundle);
         ft.add(R.id.container_body, fragment);
-        ft.addToBackStack(fragment.getClass().getSimpleName());
+        ft.replace(R.id.container_body, fragment);
         ft.commit();
     }
 }
